dbcd36958c126213a59c1fcad653a7626ea41358
mohamad <eng.muhamed.alajam@gmail.com>
2020-10-17 01:25:48 -0700

    add item and menu added

--- app/src/main/java/com/fp/shoppinglist/MainActivity.java
+++ app/src/main/java/com/fp/shoppinglist/MainActivity.java
@@ -1,10 +1,17 @@
 package com.fp.shoppinglist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager( new LinearLayoutManager(this));
 
 
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.add_item){
+            Intent i  = new Intent(this , addItems.class);
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
